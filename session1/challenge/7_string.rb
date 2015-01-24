08f2e7993a7c35c654614b5492ef7ea8dc6bd0d5
Rizwan Ali <rizcube@gmail.com>
2015-01-24 18:33:45 +0000

    fixed challenge 7

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -10,20 +10,59 @@ def pirates_say_arrrrrrrrr(string)
 	index = 0
 	add_next = false
 	string = string.split("").each do |letter|
-		current_char = string[index]
-		#puts letter[index]
-		newString << current_char if add_next
-			add_next = (current_char == "r" || current_char=="R")
+		if letter == "r" || letter =="R"
+			#puts string[index], index
+			if index + 1 < string.size
+			#if index < string.size
+				add_next= index + 1
+				newString << string[add_next]
+			end
+		
+		end
+
+		
+
+		
+		
+
+			#puts add_next
+			#puts letter, index
+		
+			#string.index(letter)
+		
+
+
+		# if letter == "r" || letter == "R"
+		# 	string.index(letter)
+		# 	#puts index
+			
+			
+
+
+			
+		
+		#print string.index(letter),letter
+
+		# if letter == "r" || "R"
+		# 	puts string.index(letter)
+
+		# #print letter, string.index(letter)
+		# end
+
+
+
+		#newString << current_char if add_next
+		#	add_next = (current_char == "r" || current_char=="R")
 		
 		
 		#puts letter
 		index += 1
 	end
-	puts newString
+	return newString
 
 end
 
 
-pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
-pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
-pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
+puts pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
+puts pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
+puts pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
